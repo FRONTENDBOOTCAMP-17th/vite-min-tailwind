351a72626764e498e2abdd7e1b6544d1831925f0
djsy01 <dlsgh8884@naver.com>
2026-02-10 09:17:49 +0900

    set up

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-import "./style.css"
+import "./style.css";
+
+const sections = [
+  "header",
+  "banner",
+  "about",
+  "download",
+  "qna",
+  "download2",
+  "apply",
+  "footer",
+];
+
+async function loadSection(name) {
+  const el = document.getElementById(name);
+  const res = await fetch(`./src/sections/${name}.html`);
+  el.innerHTML = await res.text();
+}
+
+sections.forEach(loadSection);
